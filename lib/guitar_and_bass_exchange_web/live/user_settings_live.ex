19e85a1324f7f8a59efa7bcc43fce9e41bb14457
Matt Taggart <mtaggart89@gmail.com>
2024-10-08 00:23:49 -0700

    add shell settings page

--- lib/guitar_and_bass_exchange_web/live/user_settings_live.ex
+++ lib/guitar_and_bass_exchange_web/live/user_settings_live.ex
@@ -5,69 +5,180 @@ defmodule GuitarAndBassExchangeWeb.UserSettingsLive do
 
   def render(assigns) do
     ~H"""
-    <.header class="text-center">
-      Account Settings
-      <:subtitle>Manage your account email address and password settings</:subtitle>
-    </.header>
+    <div class="flex flex-col h-screen">
+      <nav class="border-b border-gray-100 px-5 py-4 flex items-center justify-between gap-8 bg-white z-10">
+        <div class="flex items-center gap-4">
+          <.link href={~p"/"}>
+            <h1 class="text-brand text-xl">Guitar And Bass Exchange</h1>
+          </.link>
+          <div class="flex items-center gap-2">
+            <div class="flex items-center gap-1.5">
+              <.icon name="hero-map-pin-solid" class="h-7.5 w-7.5 text-blue-500" />
+              <div class="text-blue-500">Phoenix, AZ + 30 miles</div>
+            </div>
+            <div class="text-gray-500 text-[0.85rem] text-md hover:underline hover:text-gray-700 cursor-pointer">
+              update
+            </div>
+          </div>
+        </div>
+        <div class="flex items-baseline gap-4">
+          <%= if @current_user do %>
+            <sl-dropdown>
+              <button slot="trigger" class="cursor-pointer">
+                <sl-avatar label="User avatar" style="--size: 2.5rem;"></sl-avatar>
+              </button>
+              <sl-menu style="max-width: 200px;">
+                <sl-menu-item value="email" role="none" class="unstyled-menu-item">
+                  <%= @current_user.email %>
+                </sl-menu-item>
+                <div class="h-px w-full bg-gray-200"></div>
+                <.link
+                  href={~p"/users/settings"}
+                  class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+                >
+                  <sl-menu-item value="Logout">Settings</sl-menu-item>
+                </.link>
+                <.link
+                  href={~p"/users/log_out"}
+                  method="delete"
+                  class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+                >
+                  <sl-menu-item value="delete">Logout</sl-menu-item>
+                </.link>
+              </sl-menu>
+            </sl-dropdown>
+          <% else %>
+            <a href="/users/log_in" class="text-blue-700 hover:text-blue-900">Sign In</a>
+            <a href="/users/register">
+              <button
+                type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 focus:outline-none"
+              >
+                Sign Up
+              </button>
+            </a>
+          <% end %>
+        </div>
+      </nav>
+      <div class="flex flex-1 overflow-hidden">
+        <aside class="w-1/6 border-r border-gray-100 overflow-y-auto">
+          <div class="p-4">
+            <h3 class="text-xs font-semibold text-gray-500 uppercase tracking-wide mb-2 mt-4">
+              Settings
+            </h3>
+            <a
+              href="#"
+              class="block px-3 py-2 text-gray-700 hover:bg-gray-100 rounded"
+              phx-click="toggle_view"
+              phx-value-view="account"
+            >
+              Account
+            </a>
+            <a
+              href="#"
+              class="block px-3 py-2 text-gray-700 hover:bg-gray-100 rounded"
+              phx-click="toggle_view"
+              phx-value-view="billing"
+            >
+              Billing
+            </a>
+          </div>
+        </aside>
+        <main class="flex-1 overflow-y-auto p-8">
+          <div class="max-w-2xl w-full mx-auto">
+            <%= if @current_view == :account do %>
+              <.header class="text-center mb-8">
+                Account Settings
+                <:subtitle>Manage your account email address and password settings</:subtitle>
+              </.header>
 
-    <div class="space-y-12 divide-y">
-      <div>
-        <.simple_form
-          for={@email_form}
-          id="email_form"
-          phx-submit="update_email"
-          phx-change="validate_email"
-        >
-          <.input field={@email_form[:email]} type="email" label="Email" required />
-          <.input
-            field={@email_form[:current_password]}
-            name="current_password"
-            id="current_password_for_email"
-            type="password"
-            label="Current password"
-            value={@email_form_current_password}
-            required
-          />
-          <:actions>
-            <.button phx-disable-with="Changing...">Change Email</.button>
-          </:actions>
-        </.simple_form>
-      </div>
-      <div>
-        <.simple_form
-          for={@password_form}
-          id="password_form"
-          action={~p"/users/log_in?_action=password_updated"}
-          method="post"
-          phx-change="validate_password"
-          phx-submit="update_password"
-          phx-trigger-action={@trigger_submit}
-        >
-          <input
-            name={@password_form[:email].name}
-            type="hidden"
-            id="hidden_user_email"
-            value={@current_email}
-          />
-          <.input field={@password_form[:password]} type="password" label="New password" required />
-          <.input
-            field={@password_form[:password_confirmation]}
-            type="password"
-            label="Confirm new password"
-          />
-          <.input
-            field={@password_form[:current_password]}
-            name="current_password"
-            type="password"
-            label="Current password"
-            id="current_password_for_password"
-            value={@current_password}
-            required
-          />
-          <:actions>
-            <.button phx-disable-with="Changing...">Change Password</.button>
-          </:actions>
-        </.simple_form>
+              <sl-card class="mb-8 w-full">
+                <.simple_form
+                  for={@email_form}
+                  id="email_form"
+                  phx-submit="update_email"
+                  phx-change="validate_email"
+                >
+                  <.input field={@email_form[:email]} type="email" label="Email" required />
+                  <.input
+                    field={@email_form[:current_password]}
+                    name="current_password"
+                    id="current_password_for_email"
+                    type="password"
+                    label="Current password"
+                    value={@email_form_current_password}
+                    required
+                  />
+                  <:actions>
+                    <.button phx-disable-with="Changing...">Change Email</.button>
+                  </:actions>
+                </.simple_form>
+              </sl-card>
+
+              <sl-card class="w-full">
+                <.simple_form
+                  for={@password_form}
+                  id="password_form"
+                  action={~p"/users/log_in?_action=password_updated"}
+                  method="post"
+                  phx-change="validate_password"
+                  phx-submit="update_password"
+                  phx-trigger-action={@trigger_submit}
+                >
+                  <input
+                    name={@password_form[:email].name}
+                    type="hidden"
+                    id="hidden_user_email"
+                    value={@current_email}
+                  />
+                  <.input
+                    field={@password_form[:password]}
+                    type="password"
+                    label="New password"
+                    required
+                  />
+                  <.input
+                    field={@password_form[:password_confirmation]}
+                    type="password"
+                    label="Confirm new password"
+                  />
+                  <.input
+                    field={@password_form[:current_password]}
+                    name="current_password"
+                    type="password"
+                    label="Current password"
+                    id="current_password_for_password"
+                    value={@current_password}
+                    required
+                  />
+                  <:actions>
+                    <.button phx-disable-with="Changing...">Change Password</.button>
+                  </:actions>
+                </.simple_form>
+              </sl-card>
+            <% else %>
+              <.header class="text-center mb-8">
+                Billing
+                <:subtitle>Manage your credit card info & payment details</:subtitle>
+              </.header>
+
+              <sl-card class="mb-8 w-full">
+                <.simple_form
+                  for={@billing_form}
+                  id="email_form"
+                  phx-submit="update_billing"
+                  phx-change="validate_billing"
+                >
+                  <.input field={@billing_form[:first_name]} label="First Name" required />
+                  <.input field={@billing_form[:last_name]} label="Last Name" required />
+                  <:actions>
+                    <.button phx-disable-with="Changing...">Update Address</.button>
+                  </:actions>
+                </.simple_form>
+              </sl-card>
+            <% end %>
+          </div>
+        </main>
       </div>
     </div>
     """
@@ -98,7 +209,9 @@ defmodule GuitarAndBassExchangeWeb.UserSettingsLive do
       |> assign(:current_email, user.email)
       |> assign(:email_form, to_form(email_changeset))
       |> assign(:password_form, to_form(password_changeset))
+      |> assign(:billing_form, to_form(%{"first_name" => "", "last_name" => ""}))
       |> assign(:trigger_submit, false)
+      |> assign(:current_view, :account)
 
     {:ok, socket}
   end
@@ -163,5 +276,26 @@ defmodule GuitarAndBassExchangeWeb.UserSettingsLive do
       {:error, changeset} ->
         {:noreply, assign(socket, password_form: to_form(changeset))}
     end
+  end
+
+  def handle_event("toggle_view", params, socket) do
+    %{"view" => view} = params
+
+    {:noreply, assign(socket, :current_view, String.to_atom(view))}
+  end
+
+  def handle_event("validate_billing", %{"billing" => billing_params}, socket) do
+    billing_form =
+      billing_params
+      |> Map.put(:action, :validate)
+      |> to_form()
+
+    {:noreply, assign(socket, billing_form: billing_form)}
+  end
+
+  def handle_event("update_billing", %{"billing" => _billing_params}, socket) do
+    # Here you would typically update the billing information in your database
+    # For now, we'll just display a flash message
+    {:noreply, put_flash(socket, :info, "Billing information updated successfully.")}
   end
 end
