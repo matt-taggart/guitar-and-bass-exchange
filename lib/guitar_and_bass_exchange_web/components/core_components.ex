9ca37f932d935817b640cbb30c92ed1c4f8fa804
Matt Taggart <mtaggart89@gmail.com>
2024-10-20 18:43:12 -0700

    add user settings component

--- lib/guitar_and_bass_exchange_web/components/core_components.ex
+++ lib/guitar_and_bass_exchange_web/components/core_components.ex
@@ -19,6 +19,8 @@ defmodule GuitarAndBassExchangeWeb.CoreComponents do
   alias Phoenix.LiveView.JS
   import GuitarAndBassExchangeWeb.Gettext
 
+  use GuitarAndBassExchangeWeb, :verified_routes
+
   @doc """
   Renders a modal.
 
@@ -732,6 +734,99 @@ defmodule GuitarAndBassExchangeWeb.CoreComponents do
     >
       <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
     </svg>
+    """
+  end
+
+  def navbar(assigns) do
+    ~H"""
+    <nav class="border-b border-gray-100 px-5 py-4 flex items-center justify-between gap-8 bg-white z-10">
+      <div class="flex items-center gap-4">
+        <a href="/">
+          <h1 class="text-brand text-xl font-semibold">Guitar And Bass Exchange</h1>
+        </a>
+        <div class="mx-10">
+          <div class="relative">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <svg class="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
+                <path
+                  d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </span>
+            <input
+              type="text"
+              class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+        <div class="flex items-center gap-2">
+          <div class="flex items-center gap-1.5">
+            <.icon name="hero-map-pin-solid" class="h-7.5 w-7.5 text-blue-500" />
+            <div class="text-blue-500">Phoenix, AZ</div>
+          </div>
+          <div class="text-gray-500 text-[0.85rem] text-md hover:underline hover:text-gray-700 cursor-pointer">
+            update
+          </div>
+        </div>
+      </div>
+      <div class="flex items-baseline gap-4">
+        <%= if @current_user do %>
+          <sl-button variant="default" size="small">
+            <sl-icon slot="prefix" name="chat-left-text"></sl-icon>
+            Messages
+          </sl-button>
+          <.link href={~p"/users/#{@current_user.id}/posts"}>
+            <sl-button variant="default" size="small">
+              <sl-icon slot="prefix" name="file-earmark-plus"></sl-icon>
+              Posts
+            </sl-button>
+          </.link>
+          <sl-button variant="default" size="small">
+            <sl-icon slot="prefix" name="bookmark"></sl-icon>
+            Favorites
+          </sl-button>
+          <sl-dropdown>
+            <button slot="trigger" class="cursor-pointer">
+              <sl-avatar label="User avatar" style="--size: 2.5rem;"></sl-avatar>
+            </button>
+            <sl-menu style="max-width: 200px;">
+              <sl-menu-item value="email" role="none" class="unstyled-menu-item">
+                <%= @current_user.email %>
+              </sl-menu-item>
+              <div class="h-px w-full bg-gray-200"></div>
+              <.link
+                href={~p"/users/settings"}
+                class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+              >
+                <sl-menu-item value="Logout">Settings</sl-menu-item>
+              </.link>
+              <.link
+                href={~p"/users/log_out"}
+                method="delete"
+                class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+              >
+                <sl-menu-item value="delete">Logout</sl-menu-item>
+              </.link>
+            </sl-menu>
+          </sl-dropdown>
+        <% else %>
+          <a href="/users/log_in" class="text-blue-700 hover:text-blue-900">Sign In</a>
+          <a href="/users/register">
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 focus:outline-none"
+            >
+              Sign Up
+            </button>
+          </a>
+        <% end %>
+      </div>
+    </nav>
     """
   end
 end
